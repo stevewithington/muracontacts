39b82794f0c4f3906c9cffdc37ce23f1bfb3518e
Steve Withington <stephenwithington@gmail.com>
2016-09-19 13:42:30 -0500

    basic

--- assets/js/dist/muracontacts.js
+++ assets/js/dist/muracontacts.js
@@ -10,18 +10,66 @@ this["muracontacts"]["templates"]["example"] = window.mura.Handlebars.template({
 },"useData":true});;Mura.DisplayObject.muracontacts=Mura.UI.extend({
   render:function(){
     // objectparams are available under 'this.context.{yourVar}'
-
-    this.example();
-    // Mura(this.context.targetEl).html(muracontacts.templates.example({exampleVar:"hello steve!"}));
-
-    //render templates
-
-    m('.btn-delete').on('click', function(e){
-      return confirm('Are you sure?') ? true : e.preventDefault();
-    });
+    this.setup();
   }
 
-  , example:function() {
-    Mura(this.context.targetEl).html(muracontacts.templates.example({exampleVar:"hello steve!"}));
+  , setup:function() {
+    this.queryParams=this.getQueryStringParams(location.search);
+    this.queryParams.mcaction=this.queryParams.mcaction || 'list';
+
+    if(this.queryParams.mcaction=='list'){
+      this.renderList();
+    } else {
+      this.renderForm();
+    }
+
   }
+
+  , renderList:function(){
+      Mura(this.context.targetEl).html(muracontacts.templates.example({exampleVar:"I'm a list"}));
+      this.wireUpMarkup();
+  }
+
+  , renderForm:function(){
+      Mura(this.context.targetEl).html(muracontacts.templates.example({exampleVar:"I'm a form"}));
+      this.wireUpMarkup();
+  }
+
+  , wireUpMarkup:function(){
+      Mura(this.context.targetEl).find('.btn-delete').on('click', function(e){
+        return confirm('Are you sure?') ? true : e.preventDefault();
+      });
+  }
+
+  ,getQueryStringParams:function(queryString) {
+	    var params = {};
+	    var e,
+	        a = /\+/g,  // Regex for replacing addition symbol with a space
+	        r = /([^&;=]+)=?([^&;]*)/g,
+	        d = function (s) { return decodeURIComponent(s.replace(a, " ")); };
+
+	        if(queryString.substring(0,1)=='?'){
+	        	var q=queryString.substring(1);
+	        } else {
+	        	var q=queryString;
+	        }
+
+
+	    while (e = r.exec(q))
+	       params[d(e[1]).toLowerCase()] = d(e[2]);
+
+	    return params;
+	}
+
+  // ,registerHelpers: function() {
+  //   var self = this;
+  //
+  //   Mura.Handlebars.registerHelper('eachColRow',function(row, columns, options) {
+  //     var ret = "";
+  //     for(var i = 0;i < columns.length;i++) {
+  //       ret = ret + options.fn(row[columns[i].column]);
+  //     }
+  //     return ret;
+  //   });
+  // }
 });
--- assets/js/src/muracontacts.js
+++ assets/js/src/muracontacts.js
@@ -5,14 +5,52 @@ Mura.DisplayObject.muracontacts=Mura.UI.extend({
   }
 
   , setup:function() {
-    Mura(this.context.targetEl).html(muracontacts.templates.example({exampleVar:"hello steve!"}));
+    this.queryParams=this.getQueryStringParams(location.search);
+    this.queryParams.mcaction=this.queryParams.mcaction || 'list';
 
-    // move into wherever needed
-    m('.btn-delete').on('click', function(e){
-      return confirm('Are you sure?') ? true : e.preventDefault();
-    });
+    if(this.queryParams.mcaction=='list'){
+      this.renderList();
+    } else {
+      this.renderForm();
+    }
 
   }
+
+  , renderList:function(){
+      Mura(this.context.targetEl).html(muracontacts.templates.example({exampleVar:"I'm a list"}));
+      this.wireUpMarkup();
+  }
+
+  , renderForm:function(){
+      Mura(this.context.targetEl).html(muracontacts.templates.example({exampleVar:"I'm a form"}));
+      this.wireUpMarkup();
+  }
+
+  , wireUpMarkup:function(){
+      Mura(this.context.targetEl).find('.btn-delete').on('click', function(e){
+        return confirm('Are you sure?') ? true : e.preventDefault();
+      });
+  }
+
+  ,getQueryStringParams:function(queryString) {
+	    var params = {};
+	    var e,
+	        a = /\+/g,  // Regex for replacing addition symbol with a space
+	        r = /([^&;=]+)=?([^&;]*)/g,
+	        d = function (s) { return decodeURIComponent(s.replace(a, " ")); };
+
+	        if(queryString.substring(0,1)=='?'){
+	        	var q=queryString.substring(1);
+	        } else {
+	        	var q=queryString;
+	        }
+
+
+	    while (e = r.exec(q))
+	       params[d(e[1]).toLowerCase()] = d(e[2]);
+
+	    return params;
+	}
 
   // ,registerHelpers: function() {
   //   var self = this;
